7b24a0f91775581bbae179f0fd9fd8b3fdd13b9e
Sameer Aminu <sameeraminuumar521@gmail.com>
2026-05-30 01:09:48 +0100

    Added p02, p03 and p04 to ch08

---
+++ projects/ch08/p02_repeat_digit_modified2.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+/* C99 only */
+int main(void) {
+  int i, digit, repeated_digits[10] = {0};
+  long n;
+
+  printf("Enter a number: ");
+  scanf("%ld", &n);
+
+  while (n > 0) {
+    digit = n % 10;
+    repeated_digits[digit]++;
+    n /= 10;
+  }
+
+  printf("Digit:      0 1 2 3 4 5 6 7 8 9\n");
+
+  printf("Occurences: ");
+  for (i = 0; i < 10; i++)
+    printf("%d ", repeated_digits[i]);
+  printf("\n");
+
+  return 0;
+}
